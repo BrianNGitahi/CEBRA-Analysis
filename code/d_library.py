5846b766cb3492a9f93d99717ce4023ca54865c7
brian.gitahi@alleninstitute.org <brian.gitahi@alleninstitute.org>
2024-03-23 02:00:02 +0000

    CO 22/03

--- code/d_library.py
+++ code/d_library.py
@@ -12,6 +12,9 @@ from scipy.integrate import solve_ivp
 import cebra.datasets
 from cebra import CEBRA
 import torch
+import matplotlib.gridspec as gridspec
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 from matplotlib.collections import LineCollection
 import sklearn.linear_model
@@ -49,6 +52,55 @@ def get_embed(input, dimension=3):
     model.fit(input)
     embedding = model.transform(input)
     return model, embedding
+#--------------------------------------------------------------------
+
+# version 2 of get embed -- will align them later: used in pca_cebra_comp
+def base_embed(input, temp=1, dimension=3, lr = 0.01, d=0.1):
+
+    # build CEBRA time model
+    model = CEBRA(model_architecture='offset1-model',
+                         batch_size=512,
+                         learning_rate=int(lr),
+                         temperature=int(temp),
+                         output_dimension = int(dimension),
+                         max_iterations=1000,
+                         distance='euclidean',
+                         delta=int(d),
+                         conditional='delta',
+                         device='cuda_if_available',
+                         num_hidden_units=64,
+                         verbose=True,
+                         time_offsets=1)
+
+    model.fit(input)
+    embedding = model.transform(input)
+    return model, embedding
+#--------------------------------------------------------------------
+# version 3 of get embed: used in Temp analysis for the learnable temp
+
+# function to get an embedding    
+def base_embed_l(input, temp=1, dimension=3, lr = 0.01):
+# build CEBRA time model
+    fixed_temp_model = CEBRA(model_architecture='offset1-model',
+                            batch_size=512,
+                            learning_rate=int(lr),
+                            temperature_mode='auto',
+                            min_temperature=0.1,
+                            delta=0.5,
+                            output_dimension = int(dimension),
+                            max_iterations=iterations,
+                            distance='euclidean',
+                            conditional='delta',
+                            device='cuda_if_available',
+                            num_hidden_units=64,
+                            verbose=True,
+                            time_offsets=1)
+
+    fixed_temp_model.fit(input)
+    embedding = fixed_temp_model.transform(input)
+    return fixed_temp_model, embedding
+
+
 #--------------------------------------------------------------------
 
 # function to get model score + embedding
@@ -106,7 +158,7 @@ def get_r2(dataset, dimensions=[2,3,8]):
 #--------------------------------------------------------------------
 # function that plots R2 vs dimensionality for input dataset
 # loop over datasets and get their r2 scores at the 3 dimensions
-def r2_vs_dimension(datasets,labels, dimensionality=[2,3,8], ):
+def r2_vs_dimension(datasets,labels,dimensionality=[2,3,8]):
     # define list to hold the sequence of r2 scores
     r2_sequence =[]
 
@@ -166,6 +218,37 @@ def plot_circle(x,y,z=0):
     plt.show()
 
 #--------------------------------------------------------------------
+
+# function that makes the circle and adds the noise and time/theta displacement
+def noisy_circle(a=1, b=0.1, n_points=100):
+
+    # tau is uniformly distributed noise between (-0.5, 0.5)
+    tau = a*np.random.uniform(-0.5,0.5, size=100)
+
+    # Create an array of angles from 0 to 2*pi
+    theta = np.linspace(0, 2*np.pi, n_points)
+
+    # n is zero-mean additive Gaussian noise
+    n = np.random.normal(scale=b,size=100)
+
+    # Define the center and radius of the circle
+    center = (0, 0)
+    radius = 1
+
+    # Calculate the x and y coordinates of the circle with the additive noise
+    x = center[0] + radius * np.cos(theta + tau) + n 
+    x = x.reshape(-1,1)
+    y = center[1] + radius * np.sin(theta + tau) + n
+    y = y.reshape(-1,1)
+
+    # add the 3rd dimension (z coordinate of 0)
+    z = np.zeros(100) 
+    z = z.reshape(-1,1)
+
+    n_circle = np.concatenate((x,y,z), axis=1)
+
+    return n_circle
+#--------------------------------------------------------------------
     
 # define a function to make a lorenz system
 def make_lorenz():    
@@ -208,4 +291,70 @@ def plot_lorenz(coords):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title('Attractor')
+#--------------------------------------------------------------------
+
+# function to produce embeddings at different timesteps of the attractor
+def embed_attractor(attractor):
+
+   # get embeddings in 2D and 3D
+   attractor_input = attractor.reshape(attractor.shape[1],attractor.shape[0])
+   model_pl2, embed_pl2 = get_embed(attractor_input, dimension=2)
+   model_pl3, embed_pl3 = get_embed(attractor_input, dimension=3)
+
+   for i in range(0,attractor.shape[1],1000):
+
+        attractor_p = attractor[:,0:i+1000]
+
+        # define the grid
+        gs = gridspec.GridSpec(2, 2, figure=plt.figure(figsize=(10,10)))
+
+        # plot portion of attractor
+        ax1 = plt.subplot(gs[0, :], projection='3d')  
+        ax1.plot(attractor_p[0], attractor_p[1], attractor_p[2], alpha=1)
+        plt.title('Timestep {}'.format(i+1000))
+
+        # plot embeddings
+        ax2 = plt.subplot(gs[1,0])
+        cebra.plot_embedding(embedding=embed_pl2[0:i+1000,:], embedding_labels='time', markersize=5, ax=ax2, title='2D')
+        ax3 = plt.subplot(gs[1,1], projection='3d')
+        cebra.plot_embedding(embedding=embed_pl3[0:i+1000,:], embedding_labels='time', markersize=5,ax=ax3, title='3D')
+        
+        plt.show()
+
+#--------------------------------------------------------------------
+
+# function to compare the top 2 PCs and CEBRA embeddings
+def pc_cebra_comp(object, n_iterations = 1):
+    
+    # Check if the input array has shape (n, m)
+    assert object.shape[1] == 3 , f"Input array must have shape 3 columns"
+
+    # define grid
+    fig0 = plt.figure(figsize=(8,4*n_iterations))
+    gs = gridspec.GridSpec(n_iterations, 2, figure=fig0)
+
+    for i in range(0,n_iterations):
+
+        # make a pca model and fit on the object
+        pca = PCA(n_components=2)
+        pca.fit(object)
+
+        # compute the PCs and get the explained variance
+        object_pca = pca.fit_transform(object)
+        explained_var = pca.explained_variance_ratio_
+
+        # compute the cebra embedding using the ideal params learned earlier
+        cebra_model, cebra_embedding = base_embed(input=object, temp=1,dimension=3, lr=0.01, d=1)
+        
+        # make a plot of the 2 PCs of the object
+        ax0 = fig0.add_subplot(gs[i,0], projection='3d')
+        ax0.scatter(object_pca[:,0], object_pca[:,1], s=0.07)
+        ax0.set_title('Top 2 PCs of Lorenz Attractor')
+
+        # plot the cebra embedding
+        ax1 = fig0.add_subplot(gs[i,1], projection='3d') 
+        cebra.plot_embedding(embedding=cebra_embedding, embedding_labels='time',ax=ax1, markersize=0.001, alpha=1, title='Lorenz Attractor Embedding')
+        plt.show()
+
+    return explained_var
 #--------------------------------------------------------------------
--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,110 @@
-""" top level run script """
+""" this script produces all the figures in the summary document"""
+#-----------------------------------------------------------------------
+#%%
+import sys
 
+import os # my addtion
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from scipy.integrate import solve_ivp
+#import joblib as jl
+! pip install 'cebra[dev,demos]' 
+import cebra.datasets
+from cebra import CEBRA
+import torch
+from scipy.integrate import solve_ivp
+
+from matplotlib.collections import LineCollection
+import pandas as pd
+
+import pickle
+import sklearn.linear_model
+from sklearn import manifold
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+
+import d_library as dl
+
+#%%
+#-----------------------------------------------------------------------
+# define results folder
+results_folder = r"/results"
+
+# Figure 1
+def fig_1():
+
+    #make circles
+    circle1 = dl.make_circle()
+    circle2 = dl.noisy_circle(a=0.03)
+    circles = [circle1, circle2]
+    # run function
+    dl.r2_vs_dimension(circles,labels=['original circle', 'noisy circle'], dimensionality=[1,2,3,4,5])
+    plt.savefig(results_folder + os.sep + "Figure 1" + ".png")
+
+# Figure 2
+def fig_2():
+
+    # make attractor
+    xyz = dl.make_lorenz()
+    # reshape it for use with cebra
+    lorenz_obj = xyz.reshape(10000,3)
+    #run function
+    dl.r2_vs_dimension(lorenz_obj,labels=['lorenz attractor'],dimensionality=[1,2,3,4,5])
+    plt.savefig(results_folder + os.sep + "Figure 2" + ".png")
+
+# Figure 3
+def fig_3():
+
+    # make attractor and compute embedding
+    xyz = dl.make_lorenz()
+    dl.embed_attractor(xyz)
+    plt.savefig(results_folder + os.sep + "Figure 3" + ".png")
+
+# Figure 4
+def fig_4():
+     pass
+
+
+# Figure 5
+def fig_5():
+
+    #make inputs
+    circle = dl.make_circle()
+    xyz = dl.make_lorenz()
+    lorenz_obj = xyz.reshape(10000,3)
+    # get their embeddings and plot them
+    lt_model_c, lt_embedding_c = base_embed_l(circle, lr=0.1)
+    lt_model_l, lt_embedding_l = base_embed_l(lorenz_obj, lr=0.01)
+    dl.plot_2embeddings(lt_embedding_c,lt_embedding_l)
+    plt.savefig(results_folder + os.sep + "Figure 5" + ".png")
+
+
+# Figure 6
+def fig_6():
+
+    xyz = dl.make_lorenz()
+    # reshape it for use with cebra
+    lorenz_obj = xyz.reshape(10000,3)
+    # run function and save result
+    dl.pc_cebra_comp(lorenz_obj, n_iterations=3)
+    plt.savefig(results_folder + os.sep + "Figure 6" + ".png")
+
+# Figure 7
+def fig_7():
+    # make attractor and plot it
+    xyz = dl.make_lorenz()
+    dl.plot_lorenz(xyz)
+    plt.savefig(results_folder + os.sep + "Figure 7" + ".png")
+
+
+
+#%%
 def run():
-    """ basic run function """
-    pass
-
+    
+    fig_6()
+    
 if __name__ == "__main__": run()
+
+# %%
